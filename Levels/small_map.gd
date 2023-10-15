06545275778bd174dab06ab4a040a05a48cf3783
Marko Žlender <mzlender@vub.hr>
2023-10-15 20:19:46 +0200

    testing Tilemap 2

--- Levels/small_map.gd
+++ Levels/small_map.gd
@@ -1,13 +1,12 @@
 extends Control
 
-var tilemap
+@onready var tilemap = $Control3/GroundTiles
 
 var save_game = SaveGame.load_game() as SaveGame
 var character = Character.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	tilemap = $Control3/GroundTiles
 	set_process_input(true)
 	character = save_game.character
 	$Control/TextureRect.texture = character.portrait
@@ -26,7 +25,10 @@ func _input(event):
 		var tile_data : TileData = tilemap.get_cell_tile_data(0, tile_mouse_position)
 		var tile_data2 : TileData = tilemap.get_cell_tile_data(1, tile_mouse_position)
 		
+		var atlas_coords: Vector2i = Vector2i(0, 2)
 		
+		# https://www.reddit.com/r/godot/comments/13k98b0/how_do_i_use_set_cell/
+		# https://www.youtube.com/watch?v=PSEPHO8ukjI
 		
 		if tile_data:
 			var tile_name = str(tile_data.get_custom_data("Tile_name"))
@@ -37,5 +39,6 @@ func _input(event):
 		if tile_data2:
 			var tile_name2 = str(tile_data2.get_custom_data("Tile_name"))
 			print("tile mouse position: " + str(tile_mouse_position) + "\n" + tile_name2)
+			tilemap.set_cell(1, tile_mouse_position + Vector2i.DOWN, 1, atlas_coords)
 		else:
 			print("NO TILE DATA!" + str(tile_mouse_position))
