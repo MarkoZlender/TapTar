e07310d7ae81678d5475bc28c0a452931f2f1b88
Marko Žlender <mzlender@vub.hr>
2023-10-16 19:32:28 +0200

    legion doesnt go on water and lava

--- Levels/small_map.gd
+++ Levels/small_map.gd
@@ -11,7 +11,7 @@ func _ready():
 	set_process_input(true)
 	character = save_game.character
 	$Control/TextureRect.texture = character.portrait
-	legion.global_position = tilemap.map_to_local(Vector2i(1,5))
+	legion.global_position = tilemap.map_to_local(Vector2i(1,0))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -24,23 +24,23 @@ func _input(event):
 		
 		var tile_mouse_position : Vector2i = tilemap.local_to_map(mouse_position)
 		
+
 		var tile_data : TileData = tilemap.get_cell_tile_data(0, tile_mouse_position)
 		var tile_data2 : TileData = tilemap.get_cell_tile_data(1, tile_mouse_position)
 		
-		var atlas_coords: Vector2i = Vector2i(0, 2)
-		
-		# https://www.reddit.com/r/godot/comments/13k98b0/how_do_i_use_set_cell/
-		# https://www.youtube.com/watch?v=PSEPHO8ukjI
 		
 		if tile_data:
 			var tile_name = str(tile_data.get_custom_data("Tile_name"))
+			var tile_wakable: bool = tile_data.get_custom_data("walkable")
 			print("tile mouse position: " + str(tile_mouse_position) + "\n" + tile_name)
+			if tile_wakable:
+				legion.global_position = tilemap.map_to_local(tile_mouse_position)
+				
 		else:
 			print("NO TILE DATA!" + str(tile_mouse_position))
 
 		if tile_data2:
 			var tile_name2 = str(tile_data2.get_custom_data("Tile_name"))
 			print("tile mouse position: " + str(tile_mouse_position) + "\n" + tile_name2)
-			tilemap.set_cell(1, tile_mouse_position + Vector2i.DOWN, 1, atlas_coords)
 		else:
 			print("NO TILE DATA!" + str(tile_mouse_position))
