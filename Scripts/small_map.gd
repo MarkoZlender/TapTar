57ca5a1b446e9399ae993ebed1de41221c4edd20
Marko Žlender <mzlender@vub.hr>
2024-01-09 18:27:23 +0100

    github

--- Scripts/small_map.gd
+++ Scripts/small_map.gd
@@ -23,7 +23,7 @@ extends Control
 var save_game = SaveGame.load_game() as SaveGame
 var character = Character.new()
 
-
+#
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	set_process_input(true)
