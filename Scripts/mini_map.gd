f1c2ab8fca22c12f7233970b620a20f42fb53411
Marko Žlender <wartney302@gmail.com>
2024-01-23 18:27:05 +0100

    cleaned and commented out map scripts

--- Scripts/mini_map.gd
+++ Scripts/mini_map.gd
@@ -2,10 +2,9 @@ extends Control
 
 @onready var tilemap = $GroundTiles
 @onready var camera_2d = $Camera2D
+
 @onready var legion = $Legions/legion
-
 @onready var legion_3 = $Legions/legion3
-
 
 @onready var enemy_legion = $EnemyLegions/enemy_legion
 @onready var enemy_legion_2 = $EnemyLegions/enemy_legion2
@@ -27,33 +26,40 @@ var character = Character.new()
 #
 # Called when the node enters the scene tree for the first time.
 func _ready():
+
+	# enabling input, event input
 	set_process_input(true)
+
+	# setting up the character
 	character = save_game.character
 	$Control/TextureRect.texture = character.portrait
+
 	# setting legion positions
 	legion.set_legion_position(Vector2i(10,1))
-	
 	legion_3.set_legion_position(Vector2i(9,1))
 
+	# adding the legion positions to the player owned tiles in the legion controller
 	legion_controller.player_owned_tiles.append(legion.get_legion_position())
-	
 	legion_controller.player_owned_tiles.append(legion_3.get_legion_position())
 
-
+	# setting enemy legion positions
 	enemy_legion.set_legion_position(Vector2i(5,5))
-	legion_controller.enemy_owned_tiles.append(enemy_legion.legion_position)
-	#enemy_legion.set_target_position(Vector2i(8,1))
 	enemy_legion_2.set_legion_position(Vector2i(6,5))
-	legion_controller.enemy_owned_tiles.append(enemy_legion_2.legion_position)
 
-	#enemy_legion_2.set_target_position(Vector2i(10,1))
+	# adding the enemy legion positions to the enemy owned tiles in the legion controller
+	legion_controller.enemy_owned_tiles.append(enemy_legion.legion_position)
+	legion_controller.enemy_owned_tiles.append(enemy_legion_2.legion_position)
 	
 	# check if the player's legion is on a tile and if it is, change the tile
 	legion_controller.check_taken_position()
 
+	# calculate the gold
 	legion_controller.calculate_gold()
+
+	# update the gold label
 	$UICanvasLayer/UI/GoldLabel.text = str(legion_controller.gold)
 
+	# debug prints
 	print("Score: "+str(legion_controller.calculate_score()))
 	print("Gold: "+str(legion_controller.gold))
 	print("Enemy gold: " + str(legion_controller.enemy_gold))
@@ -64,20 +70,24 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+
+	# updating the camera position based on the player input
 	camera_movement()
-	#legion_controller.check_taken_position()
 
 
-	
+# checks if there is input
 func _input(_event):
+	# when player gives input, check if the input is a left click and if it is, check if the player clicked
+	# on a new tile and if they did, move the legion to that tile
 	if Input.is_action_just_pressed("left_click"):
 		legion_controller.check_taken_position()
 		legion_controller.check_engagement()
 		$UICanvasLayer/UI/GoldLabel.text = str(legion_controller.gold)
 
 		
-
+	# setting up the mini menu and connecting the buttons to their functions with signals
 	if Input.is_action_just_pressed("exit") and get_node("/root/Small_map/Camera2D/MiniMenuCanvasLayer/mini_menu") == null:
+		# instancing the mini menu scene
 		var mini_menu_scene = preload("res://Levels/mini_menu.tscn")
 		var mini_menu_instance = mini_menu_scene.instantiate()
 		$Camera2D/MiniMenuCanvasLayer.add_child(mini_menu_instance)
@@ -91,14 +101,18 @@ func _input(_event):
 		mini_menu_exit_button = get_node("/root/Small_map/Camera2D/MiniMenuCanvasLayer/mini_menu/Background/ExitButton")
 		mini_menu_exit_button.pressed.connect( _on_exit_button_pressed)
 
+
 # mini menu functions ##########################################
 
+# removes the mini menu from the scene tree
 func _on_continue_button_pressed():
 	get_node("/root/Small_map/Camera2D/MiniMenuCanvasLayer/mini_menu").queue_free()
 
+# changes the scene to the main menu
 func _on_main_menu_button_pressed():
 	get_tree().change_scene_to_file("res://Levels/Menu.tscn")
 
+# exits the game
 func _on_exit_button_pressed():
 	get_tree().quit()
 
@@ -119,17 +133,26 @@ func _on_ui_end_turn():
 		if unit != null:
 			unit.set_end_turn()
 			legion_controller.check_taken_position()
-		
+	
+	# checks if legions are engaged in combat
 	legion_controller.check_engagement()
-
+	
+	# check gold and update the gold label
 	legion_controller.calculate_gold()
 	$UICanvasLayer/UI/GoldLabel.text = str(legion_controller.gold)
+
+	# check if enemy can create new legions
 	legion_controller.create_new_enemy_legion()
+
+	# checks win/lose conditions
 	winlose.turn_small_map()
-	$UICanvasLayer/UI/TurnsLabel.text = "Turns: " + str(10-winlose.turns)
 
+	# update the turns label
+	$UICanvasLayer/UI/TurnsLabel.text = "Turns: " + str(30-winlose.turns)
 
-	print("Player ownedpositions: "+str(legion_controller.player_owned_tiles))
+	# debug prints ###########################################################################
+
+	print("Player owned positions: "+str(legion_controller.player_owned_tiles))
 	print("Enemy owned positions: "+str(legion_controller.enemy_owned_tiles))
 
 	print("Player owned tiles count: "+str(legion_controller.player_owned_tiles.size()))
@@ -138,8 +161,9 @@ func _on_ui_end_turn():
 	print("Score: "+str(legion_controller.calculate_score()))
 	print("Gold: "+str(legion_controller.gold))
 	print("Enemy gold: " + str(legion_controller.enemy_gold))
-	#legion_controller.check_taken_position()
 
+	#########################################################################################
+	
 # camera movement function #####################################
 
 func camera_movement():
@@ -155,13 +179,11 @@ func camera_movement():
 ################################################################
 	
 
-
-
-
-
-
+# function which creates a new player legion
 func _on_new_legion_button_pressed():
+
 	var new_legion_position = null
+	
 	if legion_controller.gold >= 100 and legion_controller.player_owned_tiles.size() > all_legions.size():
 		for tile in legion_controller.player_owned_tiles:
 			if tile not in legion_controller.taken_positions.values():
@@ -186,3 +208,5 @@ func _on_new_legion_button_pressed():
 				break
 	else:
 		print("Not enough gold or no available tiles")
+
+
